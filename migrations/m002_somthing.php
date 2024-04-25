d9e0af45ec14b679c503f4919416b3df8bcdd422
Sekou Samassa <sksamassa@gmail.com>
2024-04-25 16:17:25 +0500

    Migrations and connection to the db, .env and application configuration

---
+++ migrations/m002_somthing.php
@@ -0,0 +1,10 @@
+<?php
+    class m002_something {
+        public function up() {
+            echo 'Applying migration';
+        }
+
+        public function down() {
+            echo 'Down migration';
+        }
+    }
